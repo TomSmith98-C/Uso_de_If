505a0eeaffe61088114a80134b0d5cb3ee30a339
Tom Smith <74121145+TomSmith98-C@users.noreply.github.com>
2021-04-22 11:19:42 -0500

    Add files via upload

---
+++ Uso_de_if/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Uso_de_if
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Digita un numero");
+            int a = int.Parse(Console.ReadLine());
+            if (a >= 23)
+            {
+                Console.WriteLine($"El numero {a} es mayor a 23");
+            }
+            else
+            {
+                Console.WriteLine($"El numero {a} es menor a 23");
+
+            }
+            if (a == 23)
+            {
+                Console.WriteLine($"El numero {a} es igual a 23");
+
+            }
+        }
+    }
+}
